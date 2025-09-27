d7a8c35844f51a88595d5e8ceb5b32b1404a6cfd
Adam Axel Augustinsky <AdamAugustinsky@gmail.com>
2025-09-27 20:22:51 -0300

    Update .config/nvim/lazy-lock.json Update .config/nvim/lazyvim.json Update .config/nvim/lua/config/keymaps.lua Update .config/nvim/lua/plugins/catppuccin.lua Update .config/nvim/lua/plugins/claude.lua

--- dot_config/nvim/lua/plugins/catppuccin.lua
+++ dot_config/nvim/lua/plugins/catppuccin.lua
@@ -1,5 +1,25 @@
 return {
   {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      integrations = {
+        bufferline = true,
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        notify = true,
+        mini = {
+          enabled = true,
+          indentscope_color = "",
+        },
+      },
+    },
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin",
